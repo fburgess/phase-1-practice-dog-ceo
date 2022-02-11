d138d8704410a1b8824053386e2e67abb59ff037
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-02-11 10:07:38 -0500

    commit tree 5f42752addf9a8469a616fd7025b051b15382d44

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+function manDom(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then (function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function (dogData){
+        console.log(dogData);
+    })
+}
